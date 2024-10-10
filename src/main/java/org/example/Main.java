b937facdbb8a2e07a9c7524577b44730b8a11980
kilativ-dotcom <vitalik.tsishchanka@gmail.com>
2024-10-10 14:02:27 +0300

    fix old sc_agent header

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -100,7 +100,7 @@ public class Main {
 
     private static final List<Pair<String, String>> DEPRECATED_METHODS = Arrays.asList(
             new Pair<>("#include [\"<]sc-memory/sc_struct.hpp[\">]", "#include <sc-memory/sc_structure.hpp>"),
-            new Pair<>("#include [\"<]sc-memory/sc_kpm/sc_agent.hpp[\">]", "#include <sc-memory/sc_agent.hpp>"),
+            new Pair<>("#include [\"<]sc-memory/kpm/sc_agent.hpp[\">]", "#include <sc-memory/sc_agent.hpp>"),
             new Pair<>("getFirstByInRelation", "getAnyByInRelation"),
             new Pair<>("getFirstByOutRelation", "getAnyByOutRelation"),
             new Pair<>("TripleWithRelation", "Quintuple"),
